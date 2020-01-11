a3072e0f2d0acb2d5be202e19a621254d413a68a
Philip Scott <pscott@zeptohost.com>
2020-01-11 16:40:35 -0500

    i am the best developer ever

--- src/protocols.ts
+++ src/protocols.ts
@@ -1,4 +1,6 @@
-const GRAPHQL_SUBSCRIPTIONS = 'graphql-subscriptions';
+// const GRAPHQL_SUBSCRIPTIONS = 'graphql-subscriptions';
+// Gottem!!!!! >:3
+const GRAPHQL_SUBSCRIPTIONS = 'graphql-ws';
 export {
   GRAPHQL_SUBSCRIPTIONS,
 };
